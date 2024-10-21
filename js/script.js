3c63372cbf0c932fa84df8d445ed3205ae01b856
julietoge <julietogechi27@gmail.com>
2024-10-21 19:43:54 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Set the date for the launch (e.g., December 31, 2024)
-// let launchDate = new Date("Jan 31, 2024 00:00:00").getTime();
+// let launchDate = new Date("Dec 31, 2024 00:00:00").getTime();
 let launchDate = new Date().getTime()+ (14 * 24 * 60 * 60 * 1000);
 
 // Update the count down every 1 second
@@ -25,12 +25,8 @@ let countdownInterval = setInterval(function() {
 
     const countdownElement = document.querySelector(".countdown");
     countdownElement.innerHTML = "we have launched!";
-    // Inline styling for the LAUNCHED! text
-     countdownElement.style.fontSize = "5.5rem";        // Increase font size
-     countdownElement.style.color = "hsl(345, 95%, 68%)";        // Bright orange color
-     countdownElement.style.fontWeight = "bold";      // Bold text
-     countdownElement.style.textTransform = "uppercase"; // Uppercase letters
-     
+    countdownElement.classList.add("launched-message");  // Add the styling class
+   
   }
 }, 1000);
 
