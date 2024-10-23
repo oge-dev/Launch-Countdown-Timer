d2d2a9f9a5b3a9bc4073103ab81fed902399e14e
julietoge <julietogechi27@gmail.com>
2024-10-23 18:53:06 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 const countDown = () => {
-  // Set the date for the launch (e.g., December 31, 2024)
+  // Set the date for the launch (e.g., October 12, 2024)
   let launchDate = new Date("oct 12, 2024 00:00:00").getTime();
   // // let launchDate = new Date().getTime()+ (14 * 24 * 60 * 60 * 1000);
 
-  // Get the cuurent Date
+  // Get the current date
   let currentDate = new Date().getTime();
 
-  // Get the Difference btw the launchDate from currentDate
+  // Get the difference between the launchDate and currentDate
   let timeLeft = launchDate - currentDate;
 
-  // Get the second, minute, hour, day
-  let seconds = 1000; // sec in millisec
-  let minutes = 60 * seconds;
-  let hours = 60 * minutes;
-  let days = 24 * hours;
+  // Get the seconds, minutes, hours, days in milliseconds
+  let seconds = 1000; // 1 second = 1000 milliseconds
+  let minutes = 60 * seconds; // 1 minute = 60 seconds
+  let hours = 60 * minutes; // 1 hour = 60 minutes
+  let days = 24 * hours; // 1 day = 24 hours
 
-  // Time calculations for days, hours, minutes and seconds
+  // Time calculations for days, hours, minutes, and seconds
   let daysNum = Math.floor(timeLeft / days);
   let hoursNum = Math.floor((timeLeft % days) / hours);
   let minutesNum = Math.floor((timeLeft % hours) / minutes);
@@ -31,15 +31,16 @@ const countDown = () => {
   document.getElementById("seconds").textContent =
     (secondsNum < 10 ? "0" : "") + secondsNum;
 
-  // If the count down is over, display some text
+  // If the countdown is over, display a message
   if (timeLeft < 0) {
+    // Clear interval to call countDown
     clearInterval(countDown);
 
     const countdownElement = document.querySelector(".countdown");
     countdownElement.innerHTML = "we have launched!";
-    //     countdownElement.classList.add("launched-message");  // Add the styling class
+    countdownElement.classList.add("launched-message"); // Add the styling class
   }
 };
 
-// set Interval after 1000millsec
+// Set interval to call countDown every 1000 milliseconds (1 second)
 setInterval(countDown, 1000);
